cb6c1277ae072892b31bc254491fb55596859960
Brian Hie <brianhie@mit.edu>
2021-02-26 12:20:35 -0500

    epitope quality filtering

--- bin/plot_epitopes.py
+++ bin/plot_epitopes.py
@@ -5,8 +5,11 @@ if __name__ == '__main__':
     valid_ids = {}
     with open(valid_fname) as f:
         for line in f:
-            valid_id = line.rstrip().split()[0]
-            n_pubs = int(line.rstrip().split()[-2])
+            fields = line.rstrip().split()
+            valid_id = fields[0]
+            n_pubs = int(fields[-2])
+            if n_pubs < 4:
+                continue
             valid_ids[valid_id] = n_pubs
 
     fname = 'data/influenza/np_epitopes_iedb.csv'
@@ -18,9 +21,8 @@ if __name__ == '__main__':
             epitope_id = fields[0].split('/')[-1]
             if epitope_id in valid_ids:
                 mult = valid_ids[epitope_id]
-                #mult = 1
             else:
-                mult = 1
+                continue
             try:
                 start = int(fields[5])
                 end = int(fields[6])
@@ -32,13 +34,13 @@ if __name__ == '__main__':
     seq_end = 498
     namespace = fname.split('/')[-1].split('.')[0]
 
-    plt.figure(figsize=(20, 4))
-    plt.hist(positions, bins=(seq_end - seq_start + 1))
+    plt.figure(figsize=(10, 4))
+    plt.hist(positions, bins=(seq_end - seq_start)*2)
     plt.xlim([ seq_start, seq_end ])
-    plt.axvline(x=104, c='r')
-    plt.axvline(x=238, c='r')
-    plt.axvline(x=373, c='r')
-    plt.axvline(x=455, c='r')
-    plt.axvline(x=480, c='r')
+    for i in [ 104, 238, 373, 455, 480 ]:
+        plt.axvline(x=i, c='maroon')
+        print('Found {} publications at position {}'.format(
+            positions.count(i), i
+        ))
     plt.savefig(f'figures/epitopes_{namespace}.png', dpi=500)
     plt.close()
