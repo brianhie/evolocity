29de535f6bf7928fc8abe38c433afe650ef74001
Brian Hie <brianhie@mit.edu>
2021-04-09 10:52:57 -0400

    add tests

---
+++ tests/test_basic.py
@@ -0,0 +1,28 @@
+import evolocity as evo
+import numpy as np
+
+
+def test_einsum():
+    from evolocity.tools.utils import prod_sum_obs, prod_sum_var, norm
+
+    Ms, Mu = np.random.rand(5, 4), np.random.rand(5, 4)
+    assert np.allclose(prod_sum_obs(Ms, Mu), np.sum(Ms * Mu, 0))
+    assert np.allclose(prod_sum_var(Ms, Mu), np.sum(Ms * Mu, 1))
+    assert np.allclose(norm(Ms), np.linalg.norm(Ms, axis=1))
+
+
+def test_pipeline():
+    # TODO: GENERATE adata.
+
+    evo.tl.velocity_graph(adata)
+    evo.tl.velocity_embedding(adata)
+
+    evo.pl.velocity_embedding(adata)
+    evo.pl.velocity_embedding_grid(adata)
+    evo.pl.velocity_embedding_stream(adata)
+    evo.pl.velocity_contour(adata)
+
+    evo.tl.onehot_msa(adata)
+    evo.tl.resiude_scores(adata)
+    evo.pl.residue_scores(adata)
+    evo.pl.residue_categories(adata)
