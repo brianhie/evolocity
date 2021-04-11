fa7844822ed45c3875c4135b18093e3521ec637a
Brian Hie <brianhie@mit.edu>
2021-04-11 18:40:11 -0400

    remove comment

--- bin/main.sh
+++ bin/main.sh
@@ -1,17 +1,17 @@
-#python bin/flu_np.py esm1b --evolocity > np_esm1b_evolocity.log 2>&1
-#python bin/flu_np.py tape --evolocity > np_tape_evolocity.log 2>&1
-#
-#python bin/flu.py esm1b --evolocity > ha_esm1b_evolocity.log 2>&1
-#python bin/flu.py tape --evolocity > ha_tape_evolocity.log 2>&1
-#
-#python bin/gag.py esm1b --evolocity > gag_esm1b_evolocity.log 2>&1
-#python bin/gag.py tape --evolocity > gag_tape_evolocity.log 2>&1
-#
-#python bin/hiv.py esm1b --evolocity > env_esm1b_evolocity.log 2>&1
-#python bin/hiv.py tape --evolocity > env_tape_evolocity.log 2>&1
-#
-#python bin/cov.py esm1b --evolocity > spike_esm1b_evolocity.log 2>&1
-#python bin/cov.py tape --evolocity > spike_tape_evolocity.log 2>&1
+python bin/flu_np.py esm1b --evolocity > np_esm1b_evolocity.log 2>&1
+python bin/flu_np.py tape --evolocity > np_tape_evolocity.log 2>&1
+
+python bin/flu.py esm1b --evolocity > ha_esm1b_evolocity.log 2>&1
+python bin/flu.py tape --evolocity > ha_tape_evolocity.log 2>&1
+
+python bin/gag.py esm1b --evolocity > gag_esm1b_evolocity.log 2>&1
+python bin/gag.py tape --evolocity > gag_tape_evolocity.log 2>&1
+
+python bin/hiv.py esm1b --evolocity > env_esm1b_evolocity.log 2>&1
+python bin/hiv.py tape --evolocity > env_tape_evolocity.log 2>&1
+
+python bin/cov.py esm1b --evolocity > spike_esm1b_evolocity.log 2>&1
+python bin/cov.py tape --evolocity > spike_tape_evolocity.log 2>&1
 
 python bin/cyc.py esm1b --evolocity > cyc_esm1b_evolocity.log 2>&1
 python bin/cyc.py tape --evolocity > cyc_tape_evolocity.log 2>&1
