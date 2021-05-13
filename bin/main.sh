fa5f57c4b3d5155a95106e6fa6708f4440b8e33c
Brian Hie <blhie1@gmail.com>
2021-05-13 07:02:42 -0700

    merge

--- bin/main.sh
+++ bin/main.sh
@@ -1,29 +1,29 @@
-python bin/flu_np.py esm1b --evolocity > np_esm1b_evolocity.log 2>&1
-python bin/flu_np.py tape --evolocity > np_tape_evolocity.log 2>&1
-
-python bin/flu.py esm1b --evolocity > ha_esm1b_evolocity.log 2>&1
-python bin/flu.py tape --evolocity > ha_tape_evolocity.log 2>&1
-
-python bin/gag.py esm1b --evolocity > gag_esm1b_evolocity.log 2>&1
-python bin/gag.py tape --evolocity > gag_tape_evolocity.log 2>&1
-
-python bin/hiv.py esm1b --evolocity > env_esm1b_evolocity.log 2>&1
-python bin/hiv.py tape --evolocity > env_tape_evolocity.log 2>&1
+#python bin/flu_np.py esm1b --evolocity > np_esm1b_evolocity.log 2>&1
+#python bin/flu_np.py tape --evolocity > np_tape_evolocity.log 2>&1
+#
+#python bin/flu.py esm1b --evolocity > ha_esm1b_evolocity.log 2>&1
+#python bin/flu.py tape --evolocity > ha_tape_evolocity.log 2>&1
+#
+#python bin/gag.py esm1b --evolocity > gag_esm1b_evolocity.log 2>&1
+#python bin/gag.py tape --evolocity > gag_tape_evolocity.log 2>&1
+#
+#python bin/hiv.py esm1b --evolocity > env_esm1b_evolocity.log 2>&1
+#python bin/hiv.py tape --evolocity > env_tape_evolocity.log 2>&1
 
 python bin/cov.py esm1b --evolocity > spike_esm1b_evolocity.log 2>&1
 python bin/cov.py tape --evolocity > spike_tape_evolocity.log 2>&1
 
-python bin/cyc.py esm1b --evolocity > cyc_esm1b_evolocity.log 2>&1
-python bin/cyc.py tape --evolocity > cyc_tape_evolocity.log 2>&1
-
-python bin/glo.py esm1b --evolocity > glo_esm1b_evolocity.log 2>&1
-python bin/glo.py tape --evolocity > glo_tape_evolocity.log 2>&1
-
-python bin/pgk.py esm1b --evolocity > pgk_esm1b_evolocity.log 2>&1
-python bin/pgk.py tape --evolocity > pgk_tape_evolocity.log 2>&1
-
-python bin/eno.py esm1b --evolocity > eno_esm1b_evolocity.log 2>&1
-python bin/eno.py tape --evolocity > eno_tape_evolocity.log 2>&1
-
-python bin/ser.py esm1b --evolocity > ser_esm1b_evolocity.log 2>&1
-python bin/ser.py tape --evolocity > ser_tape_evolocity.log 2>&1
+#python bin/cyc.py esm1b --evolocity > cyc_esm1b_evolocity.log 2>&1
+#python bin/cyc.py tape --evolocity > cyc_tape_evolocity.log 2>&1
+#
+#python bin/glo.py esm1b --evolocity > glo_esm1b_evolocity.log 2>&1
+#python bin/glo.py tape --evolocity > glo_tape_evolocity.log 2>&1
+#
+#python bin/pgk.py esm1b --evolocity > pgk_esm1b_evolocity.log 2>&1
+#python bin/pgk.py tape --evolocity > pgk_tape_evolocity.log 2>&1
+#
+#python bin/eno.py esm1b --evolocity > eno_esm1b_evolocity.log 2>&1
+#python bin/eno.py tape --evolocity > eno_tape_evolocity.log 2>&1
+#
+#python bin/ser.py esm1b --evolocity > ser_esm1b_evolocity.log 2>&1
+#python bin/ser.py tape --evolocity > ser_tape_evolocity.log 2>&1
