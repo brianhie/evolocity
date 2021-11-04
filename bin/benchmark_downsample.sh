5d3fa02ba5ce4641e0755454b5e961f9cd44216b
Brian Hie <blhie1@gmail.com>
2021-11-04 17:53:17 +0000

    clean downsample benchmark script

--- bin/benchmark_downsample.sh
+++ bin/benchmark_downsample.sh
@@ -3,87 +3,85 @@ declare -a percentages=(75 50 25 10)
 # Uniform downsampling benchmarks.
 for percentage in ${percentages[@]}
 do
-    for seed in {2..2}
+    for seed in {0..2}
     do
-        CUDA_VISIBLE_DEVICES=0 python bin/flu_np.py esm1b --evolocity --downsample $percentage \
+        python bin/flu_np.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > np_esm1b_downsample"$percentage".log 2>&1 &
+               > np_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=1 python bin/flu.py esm1b --evolocity --downsample $percentage \
+        python bin/flu.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > h1_esm1b_downsample"$percentage".log 2>&1 &
+               > h1_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=2 python bin/gag.py esm1b --evolocity --downsample $percentage \
+        python bin/gag.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > gag_esm1b_downsample"$percentage".log 2>&1 &
+               > gag_esm1b_downsample"$percentage".log 2>&1
         
-        #CUDA_VISIBLE_DEVICES=3 python bin/cov.py esm1b --evolocity --downsample $percentage \
-        #       --seed $seed \
-        #       > cov_esm1b_downsample"$percentage".log 2>&1 &
-        
-        CUDA_VISIBLE_DEVICES=3 python bin/cyc.py esm1b --evolocity --downsample $percentage \
+        python bin/cov.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > cyc_esm1b_downsample"$percentage".log 2>&1 &
+               > cov_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=0 python bin/glo.py esm1b --evolocity --downsample $percentage \
+        python bin/cyc.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > glo_esm1b_downsample"$percentage".log 2>&1 &
+               > cyc_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=1 python bin/pgk.py esm1b --evolocity --downsample $percentage \
+        python bin/glo.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > pgk_esm1b_downsample"$percentage".log 2>&1 &
+               > glo_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=2 python bin/eno.py esm1b --evolocity --downsample $percentage \
+        python bin/pgk.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > eno_esm1b_downsample"$percentage".log 2>&1 &
+               > pgk_esm1b_downsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=3 python bin/ser.py esm1b --evolocity --downsample $percentage \
+        python bin/eno.py esm1b --evolocity --downsample $percentage \
                --seed $seed \
-               > ser_esm1b_downsample"$percentage".log 2>&1 &
-        wait
+               > eno_esm1b_downsample"$percentage".log 2>&1
+        
+        python bin/ser.py esm1b --evolocity --downsample $percentage \
+               --seed $seed \
+               > ser_esm1b_downsample"$percentage".log 2>&1
     done
 done
 
 # Weighted downsampling benchmarks.
 for percentage in ${percentages[@]}
 do
-    for seed in {2..2}
+    for seed in {0..2}
     do
-        CUDA_VISIBLE_DEVICES=0 python bin/flu_np.py esm1b --evolocity --wdownsample $percentage \
+        python bin/flu_np.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > np_esm1b_wdownsample"$percentage".log 2>&1 &
+               > np_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=1 python bin/flu.py esm1b --evolocity --wdownsample $percentage \
+        python bin/flu.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > h1_esm1b_wdownsample"$percentage".log 2>&1 &
+               > h1_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=2 python bin/gag.py esm1b --evolocity --wdownsample $percentage \
+        python bin/gag.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > gag_esm1b_wdownsample"$percentage".log 2>&1 &
+               > gag_esm1b_wdownsample"$percentage".log 2>&1
 
-        #python bin/cov.py esm1b --evolocity --wdownsample $percentage \
-        #       --seed $seed \
-        #       > cov_esm1b_wdownsample"$percentage".log 2>&1 &
+        python bin/cov.py esm1b --evolocity --wdownsample $percentage \
+               --seed $seed \
+               > cov_esm1b_wdownsample"$percentage".log 2>&1
 
-        CUDA_VISIBLE_DEVICES=3 python bin/cyc.py esm1b --evolocity --wdownsample $percentage \
+        python bin/cyc.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > cyc_esm1b_wdownsample"$percentage".log 2>&1 &
+               > cyc_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=0 python bin/glo.py esm1b --evolocity --wdownsample $percentage \
+        python bin/glo.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > glo_esm1b_wdownsample"$percentage".log 2>&1 &
+               > glo_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=1 python bin/pgk.py esm1b --evolocity --wdownsample $percentage \
+        python bin/pgk.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > pgk_esm1b_wdownsample"$percentage".log 2>&1 &
+               > pgk_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=2 python bin/eno.py esm1b --evolocity --wdownsample $percentage \
+        python bin/eno.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > eno_esm1b_wdownsample"$percentage".log 2>&1 &
+               > eno_esm1b_wdownsample"$percentage".log 2>&1
         
-        CUDA_VISIBLE_DEVICES=3 python bin/ser.py esm1b --evolocity --wdownsample $percentage \
+        python bin/ser.py esm1b --evolocity --wdownsample $percentage \
                --seed $seed \
-               > ser_esm1b_wdownsample"$percentage".log 2>&1 &
-        wait
+               > ser_esm1b_wdownsample"$percentage".log 2>&1
     done
 done
